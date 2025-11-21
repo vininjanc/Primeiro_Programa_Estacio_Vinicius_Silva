9a4c6969acb3fa97155931aa42899faadccab6fc
Vinícius Silva Morais de Sousa <243342205+vininjanc@users.noreply.github.com>
2025-11-21 04:39:06 +0000

    Arquivo no ambiente GitHub p enviar p Estácio.

---
+++ OlaJesuseoSenhor.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Olá, Jesus é o único Senhor que salva!\n");
+    return 0;
+}
--- olaLuzdomundo.c
+++ olaLuzdomundo.c
@@ -1,1 +1,6 @@
-
+#include <stdio.h>
+   
+int main() {
+    printf("Olá, Luz do mundo, Senhor Jesus!\n");
+    return 0;
+}
